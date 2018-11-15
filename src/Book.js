fe946fab92b84eeb2ecb6a19765ec41aa956bff9
Susanne <35730140+BettinaSDe@users.noreply.github.com>
2018-11-15 14:12:40 +0100

    adding src files from Create-React-App into Github

---
+++ src/Book.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+//import App from './App';
+
+
+class Book extends Component {
+    render () {
+
+      let book={book};
+      let coverImgURL = book.imageLinks
+      ? book.imageLinks.smallThumbnail
+      : "http://via.placeholder.com/128x190";
+     //let displayedThumbnail = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : '';
+
+        return (
+    <div className="book">
+    <div className="book-top">    
+    
+    
+    <div className="book-cover" style={{ width: 128, height: 193, 
+           backgroundImage: 'url("${displayedThumbnail}")' }}></div>      
+           
+                 
+           
+      <div className="book-shelf-changer">
+    
+       <select 
+        onChange={(event) => this.props.moveShelf(
+          this.props.book, event.target.value
+        )} 
+        value={this.props.book.shelf}
+                
+        >
+          <option value="move" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    </div>
+    <div className="book-title">{this.props.book.title}</div>
+    <div className="book-authors">{this.props.book.authors}</div>
+  </div>
+    );
+  }
+}
+
+export default Book; 
