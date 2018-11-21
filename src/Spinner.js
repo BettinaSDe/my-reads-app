682246478d38b23a55970868d5dfdb9e482ee2f4
Susanne <35730140+BettinaSDe@users.noreply.github.com>
2018-11-21 14:27:43 +0100

    link loading spinner async to thumbnail

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,19 +1,18 @@
 
-
-
-// I am trying to learn this from https://medium.com/@chang_yan/a-bit-more-about-react-suspense-2e05cc4e9ef8
-
 import React from 'react';
-import Book from './Book';
-//import {createResource, createCache} from 'simple-cache-provider';
-//import React, { Component } from 'react';
+import {createResource, createCache} from 'simple-cache-provider';
+
+
 
 
 
 const cache = createCache()
 const bookResource = createResource((book) => {
-  return fetch('${book-cover}/${book-cover}')
+  return fetch('${thumbnail}/${thumbnail}')
 })
+const thumbnail = ('${thumbnail}')
+
+
 
 
 class Spinner extends React.Component {
@@ -27,12 +26,13 @@ class Spinner extends React.Component {
     </React.Timeout>
   )
 
-async function bookTop ({book}) {
-  const bookhead = await book(cache, book)
+
+async function delay ({book}) {
+  const bookhead = await thumbnail(cache, book)
   return (
-    <div>{book}</div>
+    <div>{thumbnail}</div>
   )
   }
-}}
+}}   
 
 export default Spinner;
