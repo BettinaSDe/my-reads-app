1bc08c80c908a263c7dc44f794382475179f8d01
Susanne <35730140+BettinaSDe@users.noreply.github.com>
2019-01-05 18:21:13 +0100

    New SearchPage structure

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,122 +1,87 @@
-
-import React, * as react from 'react';
-import { Link } from 'react-router-dom';
-import Book from './Book';
-import * as BooksAPI from './BooksAPI';
-//import PropTypes from 'prop-types';
-//import {Provider as AlertProvider} from 'react-alert';
-//import AlertTemplate from 'react-alert-template-basic';
-
+import React, {Component} from 'react'
+import * as BooksAPI from './BooksAPI'
+import PropTypes from 'prop-types'
+import Book from './Book'
+import escapeRegExp from 'escape-string-regexp'
+//import sortBy from 'types/sort-by'
+import ErrorBoundary from './ErrorBoundary'
 
 
 
-class SearchPage extends react.Component {
-  state = {
-    query: '',
-    searchedBooks: []
+
+class SearchPage extends Component {
+static propTypes = {
+searchedBooks: PropTypes.array.isRequired,
+shelfChanger: PropTypes.func.isRequired
+}
+
+state = {
+  query: ''
+}
+
+updateQuery = (query) => {
+  this.setState({query: query.trim() })
+}
+
+
+render() {
+
+    const { books, shelfChanger } = this.props
+    const { query } = this.state 
+    
+
+ 
+    let searchedBooks 
+
+  if (query) {
+    const match = new RegExp(escapeRegExp(query), 'i')
+    searchedBooks = books.filter((book) => match.test(book.id))
+
+  } else {
+    searchedBooks = <ErrorBoundary />
   }
 
-  newSearch = (query) => {
-    this.setState({
-      query: query
-    })
-    this.newSearchedBooks(query);
-  }
-
-  newSearchedBooks = (query) => {
-    if (query) {
-      BooksAPI.search(query).then((searchedBooks) => {
-        if (searchedBooks.error) {
-          this.setState({ searchedBooks: [] });
-          this.props.alert.show(<div style={{ color: 'blue' }}>Title not found</div>)
-
-        } else {
-          this.setState({ searchedBooks: searchedBooks });
-        }
-      })
-    } else {
-      this.setState({ searchedBooks: [] });
-    }
-  }
-
-  render() {
+  //searchedBooks.sort(sortBy('name'))
 
 
-// I learned this shorthand React.Fragment syntax on: https://getstream.io/blog/react-fragments/ 
-    return ( 
-       <div>
-      <React.Fragment>  
-       <div className="search-books" />
-        <div className="search-books-bar" />
 
-          <Link to="/" className="close-search" >Close</Link>
-          <div className="search-books-input-wrapper" />
+    return (
+   
+  
+      <div className="search-books">
+         <div className="search-books-input-wrapper">           
+          <input         
+           div handler="onChange"
+            type="text" 
+            placeholder="Search by title or author" 
+            value={query}
+            onChange={(event) => this.updateQuery(
+              event.target.value)}
+              />            
+        </div>
+        
+        
       
-
-           <div> 
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              value={this.state.query}
-              onChange={(e) => this.newSearch(e.target.value)}
-            />
-            </div >
-      </React.Fragment>
-           
-          
-        
-      <React.Fragment>
-        <div className="search-books-results" />
+             
+       
+     <div>
+          <div className="search-books-results">
           <ol className="books-grid">
-            {
-
-              this.state.searchedBooks.map(searchedBook => {
-
-                return (
-
-                  <li key={searchedBook.id}>
-                    <Book
-                      book={searchedBook}
-                      shelfChanger={this.props.shelfChanger}
-
-                    />
-                  </li>
-                );
-              })
-            }
+            {searchedBooks.map(searchedBook => (<Book key={searchedBook.id} book={searchedBook} updateBookInfo={this.props.updateBookInfo} />))}
           </ol>
+        </div>
+        </div></div>
+      
+       
+      
         
-        </React.Fragment>
-      </div>
-    );
-  }
+     
+     
+      )
+    }
 }
 
 
 
-export default SearchPage;
+export default SearchPage
 
-
-
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
