e2bb4e5c7b475f81cb10257037eb7ff024ec6997
Susanne <35730140+BettinaSDe@users.noreply.github.com>
2019-01-05 20:54:03 +0100

    modified BookShelf
    slightly more hardcoded

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -21,7 +21,6 @@ const shelves = [
   }
 ];
 
-const { books, shelfChanger } = this.props;
 
 
 
@@ -30,30 +29,92 @@ class BookShelf extends Component {
 
   render() {
 
+    const { books, shelfChanger } = this.props;
+    
     
   return (
 
-    <div>
+   <div>
     <div className="bookshelf">
-      <div className="bookshelf-title" />
+      <div className="bookshelf-title = CurrentlyReading">
       <div className="bookshelf-books">
         <ol className="books-grid">
           {
-            shelves.map(item => (
+            shelves.map(book => (
+             <li key = {book.id} className="book">
 
               <BookShelf
                 books={books}
-                title={item.title}
-                shelf={item.shelf}
+                title={book.title}
+                shelf={book.shelf}
                 shelfChanger={shelfChanger}
               />
+              </li>
             ))
           }
         </ol>
       </div>
       </div>
-      </div>   
-                   
+      </div>  
+   
+               
+               
+
+
+
+    <div>           
+    <div className="bookshelf">
+      <div className="bookshelf-title = wantToRead">
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {
+            shelves.map(book => (
+             <li key = {book.id} className="book">
+
+              <BookShelf
+                books={books}
+                title={book.title}
+                shelf={book.shelf}
+                shelfChanger={shelfChanger}
+              />
+              </li>
+            ))
+          }
+        </ol>
+      </div>
+      </div>
+      </div>  
+      </div> 
+   
+
+
+
+<div>
+    <div className="bookshelf">
+      <div className="bookshelf-title = Read">
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {
+            shelves.map(book => (
+             <li key = {book.id} className="book">
+
+              <BookShelf
+                books={books}
+                title={book.title}
+                shelf={book.shelf}
+                shelfChanger={shelfChanger}
+              />
+              </li>
+            ))
+          }
+        </ol>
+      </div>
+      </div>
+      </div>  
+      </div>
+    </div>
+
+
        );
   }
 }
