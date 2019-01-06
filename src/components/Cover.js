00ec462f36b0e054aa72e99477ec6b3414d90ce6
Susanne <35730140+BettinaSDe@users.noreply.github.com>
2019-01-06 16:52:19 +0100

    separate Components folder

---
+++ src/components/Cover.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react';
+import './App.css'
+import ErrorBoundary from './ErrorBoundary';
+
+
+
+
+class Cover extends Component {
+    render() {
+
+        console.log(this.props.book);
+        
+         // log out the prop passed to this component
+        return (
+            <ErrorBoundary>
+            <img className="Cover"
+                src={this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''}
+                alt={this.props.book.title}
+            />
+            </ErrorBoundary>
+        );
+    }
+};
+
+export default Cover;
+
+
+
